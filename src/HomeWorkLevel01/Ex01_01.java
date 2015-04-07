7e09dde08eb250cd15151713c50948b9d032d50f
ruslan_lizogub <ruslanlizogub0201@gmail.com>
2015-04-07 11:23:13 +0300

    Ex01_01HelloWorld

--- src/HomeWorkLevel01/Ex01_01.java
+++
@@ -1,9 +0,0 @@
-package HomeWorkLevel01;
-
-public class Ex01_01 {
-	public static void main(String[] args) {
-        // Display "Hello World!"
-        System.out.println("Hello World!");
-    }
-}
-//test
---
+++ src/HomeWorkLevel01/Ex01_01HelloWorld.java
@@ -0,0 +1,12 @@
+package HomeWorkLevel01;
+/* Исполнитель: ruslan_lizogub
+ * Бал: 100
+ * Код решения: hYoPaf6wJUeV8zbP/Z7QGJWLBKA=
+ * Название упражнения: "Hello World"
+ * Условие: "Написать приложение, выводящее на экран сообщение "Hello world"."
+ * */
+public class Ex01_01HelloWorld {
+	public static void main(String[] args) {
+		System.out.println("Hello world");
+    }
+}
